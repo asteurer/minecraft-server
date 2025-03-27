9480121f897e19d5187c6fc4fcd75e840758033a
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-03-27 02:46:55 +0000

    fix: forgot to remove archive after upload
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- systemd_backup/backup-minecraft.sh
+++ systemd_backup/backup-minecraft.sh
@@ -7,3 +7,5 @@ bucket_name=asteurer-minecraft-backups
 tar --xz -cf $file_name --exclude=bedrock_server-* $data_dir
 
 B2_APPLICATION_KEY_ID=$B2_ID B2_APPLICATION_KEY=$B2_KEY b2 file upload $bucket_name $file_name $file_name
+
+rm $file_name
