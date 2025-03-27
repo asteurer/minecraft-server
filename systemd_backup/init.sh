b0da739cfd119e92bff8fae16640fc1b872d9287
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-03-27 02:27:13 +0000

    fix: small bug
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- systemd_backup/init.sh
+++ systemd_backup/init.sh
@@ -2,9 +2,12 @@
 folder_name=systemd_backup
 file_name=backup-minecraft
 
-cat $folder_name/$file_name.sh | sudo tee /usr/bin/$file_name.sh
 cat $folder_name/$file_name.service | sudo tee /etc/systemd/system/$file_name.service
+
 cat $folder_name/$file_name.timer | sudo tee /etc/systemd/system/$file_name.timer
+
+cat $folder_name/$file_name.sh | sudo tee /usr/bin/$file_name.sh
+sudo chmod +x /usr/bin/$file_name.sh
 
 source b2.env # This assumes the file is in the project root
 
