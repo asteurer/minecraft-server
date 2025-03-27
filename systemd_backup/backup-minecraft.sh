ee53abcc658d2502d2ae86261398b6b6b581b217
Andrew Steurer <94206073+asteurer@users.noreply.github.com>
2025-03-27 01:49:19 +0000

    fix: fixing b2 command arg
    Signed-off-by: Andrew Steurer <94206073+asteurer@users.noreply.github.com>

--- systemd_backup/backup-minecraft.sh
+++ systemd_backup/backup-minecraft.sh
@@ -6,4 +6,4 @@ bucket_name=asteurer-minecraft-backups
 
 tar --xz -cf $file_name --exclude=bedrock_server-* $data_dir
 
-B2_APPLICATION_KEY_ID=$B2_ID B2_APPLICATION_KEY=$B2_KEY b2 upload_file $bucket_name $file_name
+B2_APPLICATION_KEY_ID=$B2_ID B2_APPLICATION_KEY=$B2_KEY b2 upload_file $bucket_name $file_name $file_name
